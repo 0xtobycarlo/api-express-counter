28bc9fefbc583aa8d6163dcbf5aa5e20f34fa888
Toby Carlo <tobycarlo.c@gmail.com>
2022-05-26 16:31:37 +0100

    DELETE & ext1

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ app.listen(port, () => {
   console.log(`Server is running on https://localhost:${port}/`);
 });
 
+app.get("/", (req, res) => {
+  console.log("Got request!");
+  res.send("Hello World!");
+});
+
 let counter = 0;
 
 app.get("/counter", (req, res) => {
@@ -25,5 +30,23 @@ app.post("/counter/increment", (req, res) => {
 
 app.post("/counter/decrement", (req, res) => {
   counter--;
+  res.json({ counter: counter });
+});
+
+app.post("/counter/double", (req, res) => {
+  counter *= 2;
+  res.json({ counter: counter });
+});
+
+app.delete("/counter", (req, res) => {
+  counter = 0;
+  res.json({ counter: counter });
+});
+
+app.put("/counter", (req, res) => {
+  if (!req.query.value) {
+    res.json({ counter: counter });
+  }
+  counter = Number(req.query.value);
   res.json({ counter: counter });
 });
