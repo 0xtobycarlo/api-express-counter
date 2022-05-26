5117cd673d072ff309ec4dccc7cc4f3cbd6eede3
Toby Carlo <tobycarlo.c@gmail.com>
2022-05-26 15:58:11 +0100

    increment & decrement

--- index.js
+++ index.js
@@ -11,3 +11,19 @@ const port = 3030;
 app.listen(port, () => {
   console.log(`Server is running on https://localhost:${port}/`);
 });
+
+let counter = 0;
+
+app.get("/counter", (req, res) => {
+  res.json({ counter: counter });
+});
+
+app.post("/counter/increment", (req, res) => {
+  counter++;
+  res.json({ counter: counter });
+});
+
+app.post("/counter/decrement", (req, res) => {
+  counter--;
+  res.json({ counter: counter });
+});
